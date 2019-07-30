c56ad3ae6a8efb14f624622128bb8d62794cc09b
Markosaur <mark3pace@gmail.com>
2019-07-29 19:32:02 -0600

    Part 1 of Vanilla 1 complete

--- Part1/index.js
+++ Part1/index.js
@@ -1,20 +1,21 @@
-console.log('Hello Mark');
+// console.log('Hello Mark');
 
 // reassign value from x to o when the board is clicked
 
-const playerValues = [];
+let playerValues = [];
 const play = (clickedId) => {
     const playerSpan = document.getElementById('player');
     console.dir(playerSpan.innerText)
     const clickedElement = document.getElementById(clickedId);
     clickedElement.innerText = playerSpan.innerText
-    playerValues[clickedId] = playerSpan.innnerText
+    playerValues[clickedId] = playerSpan.innerText
 
     //horizontal
-    if(playerValues[0] !== undefined && playerValues[0]===playerValues[1] &&playerValues[1]===playerValues[2]){
-        console.log('YOLO')
-            alert(`${playerSpan.innerText} is the winner!`)
+    console.dir(playerValues[0]);
+    if(playerValues[0] !== undefined && playerValues[0] === playerValues[1] && playerValues[1] === playerValues[2]){
+        alert(`${playerSpan.innerText} is the winner!`);
     }
+
     if(playerValues[3] !== undefined && playerValues[3]===playerValues[4] &&playerValues[4]===playerValues[5]){
         alert(`${playerSpan.innerText} is the winner!`)
     }
@@ -45,10 +46,24 @@ const play = (clickedId) => {
     if(playerSpan.innerText === 'X'){
         playerSpan.innerText = 'O' 
         console.log("OOO")
-    } else if (playerSpan.innerText === 'O'){
+    } else if  (playerSpan.innerText === 'O'){
         playerSpan.innerText = 'X'
         console.log("XXX")
     }
+  
+    let boardFull = true;
+    for (let i = 0; i <= 8; i++) {
+        if (playerValues[i] === undefined) {
+            boardFull = false;
+        }
+    }
+        if (boardFull === true) {
+            alert("Cat's Game, There is no winner");
+        }
+        
+
+    console.log(playerValues);
+    
     console.dir(playerSpan.innerText)
     console.log(playerValues);
 };
